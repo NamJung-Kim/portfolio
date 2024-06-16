95b1a11fdf0ce3186058caaf8f34a02d8be01706
NamJung-Kim <83390063+NamJung-Kim@users.noreply.github.com>
2024-06-16 23:57:11 +0900

    js-home

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 const header = document.querySelector('.header');
+const home = document.querySelector('.home__container');
+
 const headerHeight = header.getBoundingClientRect().height;
+const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  // header에 페이지 아래로 스크롤시 다크 스타일링 적용
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
-  }
+  } 
 });
 
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY/ homeHeight;
+  console.log(1 - window.scrollY/ homeHeight);
+});
