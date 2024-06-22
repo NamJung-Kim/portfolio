28da5aa04f93197e17258918c4984b2173ebf629
NamJung-Kim <83390063+NamJung-Kim@users.noreply.github.com>
2024-06-22 13:10:06 +0900

    active-menu2

--- src/active_menu.js
+++ src/active_menu.js
@@ -2,12 +2,17 @@
 
 const sectionIds = ["#home", "#about", "#skills", "#work", "#testimonial", "#contact"];
 const sections = sectionIds.map(id => document.querySelector(id));
-const navItem = sectionIds.map(id => document.querySelector(`a[href="${id}"]`));
+const navItems = sectionIds.map(id => document.querySelector(`a[href="${id}"]`));
 // console.log(navItem);
 
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.98],
+};
+
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach(section => observer.observe(section));
 
@@ -19,15 +24,27 @@ function observerCallback(entries) {
     selectLastOne = 
     index === sectionIds.length - 1 &&
     entry.isIntersecting &&
-    entry.intersectionRatio >= 0.99;
+    entry.intersectionRatio >= 0.95;
   });
 
   const navIndex = selectLastOne 
   ? sectionIds.length - 1 
   : findFirstIntersecting(visibleSections);
+
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) {
+    return;
+  }
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
 }
